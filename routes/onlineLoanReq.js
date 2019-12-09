fac15ac7261f6349dcf9dee7a29faaaa67c5fb87
Nuwanthika <nuwanthikaw.17@cse.mrt.ac.lk>
2019-12-09 13:59:16 +0530

    online loan form route

---
+++ routes/onlineLoanReq.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+var conn = require('./connection');
+
+router.get('/',function(req,res,next){
+    res.render('OnlineLoan');
+
+});
