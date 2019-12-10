08ba6bcbe5fa24f434a38e0dfa9fc887f9d023b4
Nuwanthika <nuwanthikaw.17@cse.mrt.ac.lk>
2019-12-10 23:17:27 +0530

    complete online loan

--- routes/onlineLoanReq.js
+++ routes/onlineLoanReq.js
@@ -7,26 +7,35 @@ router.get('/', function (req, res, next) {
     res.render('onlineLoanReq');
 
 });
- router.post('/', function (req, res) {
-    let account_num = req.body.account_num;
-    let repayment = req.body.repayment;
-    let amount = req.body.amount;
-    //let fd_id = req.body.fd_id;
+router.post('/', function (req, res) {
+  let account_num = req.body.account_num;
+  let repayment = req.body.repayment;
+  let amount = req.body.amount;
+  //let fd_id = req.body.fd_id;
+  
+  console.log(account_num);
+
+  conn.connect(function(err) {
+      if (err) throw err;
+      console.log("Database connected successfully!");
+      // console.log(fd_id);
+      var fd_id_query = `SELECT fd_id from fixed_deposit where account_num = ${account_num}`;
+      conn.query(fd_id_query,function(err,result){
+        if (err) throw error;
+
+        const response = JSON.parse(JSON.stringify(result));
+        const fd_id = response[0].fd_id;
+  
+        var sqlquery = "INSERT INTO loan (loan_id,loan_amount,repayment_period,start_date,state,fd_id,account_num) VALUES ('1','"+amount+"','"+repayment+"','45','1','"+fd_id+"','"+account_num+"')";
+        conn.query(sqlquery, function (err, result) {
+          if (err) throw err;
+              console.log(result.affectedRows + " record inserted");
+        });
+      })
+
     
-
-conn.connect(function(err) {
-    if (err) throw err;
-    console.log("Database connected successfully!");
-    console.log(fd_id);
-    var fd_id = "SELECT fd_id from fixed_deposit where account_num = '"+account_num+"'";
-    console.log(fd_id);
-    var sqlquery = "INSERT INTO loan (loan_id,loan_amount,repayment_period,start_date,state,fd_id,account_num) VALUES ('1','"+amount+"','"+repayment+"','45','1','"+fd_id+"','"+account_num+"')";
-  conn.query(sqlquery, function (err, result) {
-    if (err) throw err;
-        console.log(result.affectedRows + " record inserted");
+    
   });
-});
- })
-//});
+})
 
-module.exports = router;
+module.exports = router
