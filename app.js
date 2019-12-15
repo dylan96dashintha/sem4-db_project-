62452478eb8aa3b769a379b722dd6708f8f7f83f
dylan96dashintha <dashinthadilan.17@cse.mrt.ac.lk>
2019-12-15 19:28:50 +0530

    fixed deposit

--- app.js
+++ app.js
@@ -3,15 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
 var empProfileRouter = require('./routes/empProfile');
 var customProfileRouter = require('./routes/customProfile');
-var createPersonalFormRouter = require('./routes/createPersonalForm');
+var createPersonalNewFormRouter = require('./routes/createPersonalNewForm');
+var createPersonalNewAccountRouter = require('./routes/createPersonalNewAccount');
+var createPersonalExistAccountRouter = require('./routes/createPersonalExistAccount');
 var createOrganizationalFormRouter = require('./routes/createOrganizationalForm');
 var normalLoanRequestRouter = require('./routes/normalLoanRequest');
+var onlineFDRouter = require('./routes/onlineFD');
+
 var app = express();
 
 // view engine setup
@@ -20,6 +25,7 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(session({secret : 'sssdddd'}))
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -29,9 +35,12 @@ app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 app.use('/empProfile', empProfileRouter);
 app.use('/customProfile', customProfileRouter);
-app.use('/createPersonalForm', createPersonalFormRouter);
+app.use('/createPersonalNewForm', createPersonalNewFormRouter);
+app.use('/createPersonalNewAccount', createPersonalNewAccountRouter);
 app.use('/createOrganizationalForm', createOrganizationalFormRouter);
+app.use('/createPersonalExistAccount', createPersonalExistAccountRouter);
 app.use('/normalLoanRequest', normalLoanRequestRouter);
+app.use('/onlineFD', onlineFDRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
---
+++ routes/onlineFD.js
@@ -0,0 +1,105 @@
+var express = require('express');
+var router = express.Router();
+var fdId = require('./generateFdId');
+var conn = require('./connection');
+
+router.get('/',function(req,res,next){
+    
+    res.render('onlineFD');
+
+});
+
+router.post('/',function(req,res){
+    let amount = req.body.amount;
+    let plan = req.body.plan;
+    let accountNum = req.body.accountNum;
+//    let username = req.session.username;
+    let uname = req.session.username;
+    let fd; 
+    console.log(plan);
+    checkValidity();
+
+    function checkValidity(){
+        conn.query(`SELECT username FROM  customer_login WHERE customer_id in (SELECT customer_id FROM account WHERE account_num = '${accountNum}') `,function(err,result){
+            if(err){
+                res.redirect('/onlineFD');
+            }else{
+                if (result.length != 0){
+                    console.log("success");
+                    conn.query(`SELECT balance FROM saving_account WHERE account_num = '${accountNum}' `,function(err,result){
+                        if (err) {
+                            res.send(err);
+                        }else{
+                            let balance = parseFloat(result[0].balance);
+                            if(balance >= amount){
+                                    checkFdId();
+                                //res.send("you can have a fd");
+                            }else{
+                                res.redirect('/onlineFD');
+                            }
+                        }
+                    });
+                }
+            }
+            
+            
+            
+        });
+    }
+
+
+
+    function checkFdId() {
+        fd = fdId();
+        console.log(fd);
+        conn.query(`SELECT fd_id FROM fixed_deposit WHERE fd_id = '${fd}'`, (err, result) => {
+            if (result.length != 0) {
+                console.log(result.length);
+                checkFdId();
+            } else {
+                updateDb();
+            }
+        });
+
+    }
+
+    function updateDb(){
+        conn.beginTransaction(function(err){
+            if(err){throw err;}
+                conn.query(`INSERT INTO fixed_deposit(fd_id,num_months,start_date,amount,net_interest,account_num) VALUES ('${fd}',${plan},curdate(),'${amount}',0,'${accountNum}')`,function(err,result){
+                    if(err){
+                        conn.rollback(function(err){
+                            res.send(err);
+
+                        });
+                    }else{
+                        conn.query(`UPDATE account SET balance =  balance - ${amount} WHERE account_num = '${accountNum}'`,function(err,result){
+                            if(err){
+                                conn.rollback(function(err){
+                                    res.send(err);
+                                });
+                            }
+                            conn.commit(function(err) {
+                                if (err) { 
+                                  conn.rollback(function() {
+                                    throw err;
+                                  });
+                                }
+                                res.send("successfully updated");
+                                console.log('Transaction Complete.');
+                                conn.end();
+                              });
+                        });
+                    }
+                });
+        });
+                
+        
+        
+    }
+
+
+});
+
+
+module.exports = router;
