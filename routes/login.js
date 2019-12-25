76cedbee4be2818531b044bae1f5729a10333dad
dylan96dashintha <dashinthadilan.17@cse.mrt.ac.lk>
2019-12-25 08:14:05 +0530

    asas

--- routes/login.js
+++ routes/login.js
@@ -4,6 +4,7 @@ var conn = require('./connection');
 //const session = require('express-session');
 
 router.get('/',function(req,res,next){
+
     res.render('login');
 
 });
@@ -23,6 +24,7 @@ router.post('/',function(req,res){
             //console.log(uname,psw,emp_id); 
             if ( username == uname && pswd == psw && emp_id != null){
                 check = true;
+
                 conn.query(`SELECT count(branch_manager.emp_id) AS counter FROM branch_manager,employee_login where branch_manager.emp_id = '${emp_id}' ` , function(err,result){
                     if (err) {
                         console.log(err);
@@ -32,7 +34,7 @@ router.post('/',function(req,res){
                             req.session.username = uname;
                             res.redirect('/customerAccount');
                         }else{
-                            req.session.emp_id = emp_id
+                            req.session.emp_id = emp_id;
                             res.redirect('/branchManagerProfile');
 
                         }
