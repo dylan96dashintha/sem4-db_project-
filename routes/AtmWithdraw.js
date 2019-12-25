3ed3a2b96e0fa485bb46c05535c0c261398bdb44
Nuwanthika <nuwanthikaw.17@cse.mrt.ac.lk>
2019-12-25 21:05:31 +0530

    complete atm withdraw transaction

--- routes/AtmWithdraw.js
+++ routes/AtmWithdraw.js
@@ -4,30 +4,39 @@ var conn = require('./connection');
 
 
 router.get('/', function (req, res, next) {
-    res.render('AtmWithdraw');
-});
+    
+    var transactionIdList = []  ;
+    var dateList = [];
+    var timeList = [];
+    var atmIdList=[];
+    var accountNumList = [];
+    var amountList = [];
 
 
  //console.log("Database connected successfully!");
- var atmwithdraw_query = `SELECT transaction_id,atm_id,account_num,date,time_transaction FROM transaction right outer join atm_withdraw using(transaction_id) `;
+ var atmwithdraw_query = `SELECT transaction_id,date,time_transaction,atm_id,account_num,amount FROM transaction right outer join atm_withdraw using(transaction_id) `;
 
  conn.query(atmwithdraw_query,function(err,result){
  if (err) throw error;
-     
-     const response = JSON.parse(JSON.stringify(result));
-     for(x=0;x<response.length;x++){
-     var transaction_id= response[x].transaction_id;
-     var atm_id=response[x].atm_id;
-     var account_num=response[x].account_num;
-     var date=response[x].date;
-     var time_transaction=response[x].time_transaction;
-     //console.log(transaction_id,atm_id,account_num,date,time_transaction);
-     
+
+ const response = JSON.parse(JSON.stringify(result));  
+ 
+    for(x=0 ; x<response.length ; x++){
+        transactionIdList.push(response[x].transaction_id);
+        dateList.push(response[x].date);
+        timeList.push(response[x].time_transaction);
+        atmIdList.push(response[x].atm_id,);
+        accountNumList.push(response[x].account_num,);
+        amountList.push(response[x].amount);
+
+
      }
+
+     res.render('AtmWithdraw',{tranc_id : transactionIdList,date :dateList , time :timeList,atm:atmIdList, acc_num:accountNumList ,  amm :amountList });
 
  });
 
- 
+});
 
     
    
